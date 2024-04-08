1eeafc904146b30ca23dcfed094b51ef185754fd
radackj <josh.radack@gmail.com>
2024-04-08 13:15:57 -0400

    Created rmd render script

---
+++ scripts/rmd_renderer.r
@@ -0,0 +1,8 @@
+################################################################################
+## Program: rmarkdown-ppt-demo/scripts/rmd_renderer.r
+## Date: 2024-04-08
+## Created by: Josh Radack
+## Description: Knits R Markdown demo files
+################################################################################
+rm(list=ls())
+

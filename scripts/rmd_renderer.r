a5ad15f21687c41615aff8d3791a8b856e28f611
radackj <josh.radack@gmail.com>
2024-04-08 13:16:15 -0400

    Added render commands for powerpoint and tutorial

--- scripts/rmd_renderer.r
+++ scripts/rmd_renderer.r
@@ -6,3 +6,16 @@
 ################################################################################
 rm(list=ls())
 
+# R Markdown HTML Tutorial
+rmarkdown::render(
+  input = "scripts/rmd_ppt_tutorial.rmd",
+  output_dir = "output",
+  output_file = "rmd_ppt_tutorial.html"
+)
+
+# R Markdown Demo PowerPoint
+rmarkdown::render(
+  input = "scripts/rmd_ppt_demo.rmd",
+  output_dir = "output",
+  output_file = "rmd_ppt_demo.pptx"
+)
